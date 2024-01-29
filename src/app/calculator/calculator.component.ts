bdb6ecd971b8603cdf3aac83f41f086edb83bd56
DM <82635721+makarenko290102@users.noreply.github.com>
2024-01-29 16:59:57 +1000

    tsChange1

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -5,34 +5,87 @@ import { Component } from '@angular/core';
   templateUrl: './calculator.component.html',
   styleUrl: './calculator.component.css'
 })
-export class CalculatorComponent {
-  
-  public first: number = 1;
-  public second: number = 1;
 
-  public operation: string = '+';
-
-  public operations: string[] = ['+', '-', '*', '/'];
-
-  public result?: number;
-
-  public calc(){
-if(this.operation){
-  switch(this.operation){
-    case '+':
-      this.result = this.first + this.second;
-      break;
-    case '-':
-      this.result = this.first - this.second;
-      break;
-    case '*':
-      this.result = this.first * this.second;
-      break;
-    case '/':
-      this.result = this.first / this.second
-  }
+interface ICalcGroup {
+  first: CalcVar,
+  second: CalcVar,
+  iperation: EnumCalcOperations
 }
-  }
+
+interface CalcVar{
+  value: number
+  modificator: EnumCalcModific
+}
+
+enum EnumCalcOperations {
+  plus = '+',
+  minus = '-',
+  multiply = '*',
+  divide = '/'
+}
+
+enum EnumCalcModific {
+  none = 'none',
+  sin = 'sin',
+  cos = 'cos',
+  square = 'square'
+}
+
+
+
+
+
+
+export class CalculatorComponent {
+
+  public calcOperations = EnumCalcOperation
+  public calcModific = EnumCalcModific
+  public calcGroup : ICalcGroup[] = [
+    {
+    first:{
+      value: 10,
+      modificator: EnumCalcModific.none
+    },
+    second: {
+      value: 20,
+      modificator: EnumCalcModific.none
+    },
+    operations: EnumCalcOperations.plus
+    } 
+  ]
+
+  public logHistory: string[] = [];
+
+  public operationBetwGroups: EnumCalcOperations//[] = [];
+
+  
+  //public first: number = 1;
+  //public second: number = 1;
+
+  //public operation: string = '+';
+
+  //public operations: string[] = ['+', '-', '*', '/'];
+
+  //public result?: number;
+
+  //public calc(){
+//if(this.operation){
+  //switch(this.operation){
+   // case '+':
+      //this.result = this.first + this.second;
+    //  break;
+    //case '-':
+     // this.result = this.first - this.second;
+     // break;
+    //case '*':
+      //this.result = this.first * this.second;
+     // break;
+    //case '/':
+     // this.result = this.first / this.second
+  
+ // }
+//}
+// }
 
 }
 
